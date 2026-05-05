dcde8a915a01c65f1e9e1f6650b569a95f345b6c
hasdpk <hasdpk@gmail.com>
2026-05-05 15:43:43 +0100

    refactor(installer): streamline InstallerFactory

--- src/InstallerFactory.php
+++ src/InstallerFactory.php
@@ -7,32 +7,26 @@ use RubenMartinDev\PrestaShopModuleInstaller\Handler\Database\DatabaseHandlerFac
 use RubenMartinDev\PrestaShopModuleInstaller\Handler\Database\DatabaseHandlerInterface;
 use RubenMartinDev\PrestaShopModuleInstaller\Handler\Hook\HookHandlerFactory;
 use RubenMartinDev\PrestaShopModuleInstaller\Handler\Hook\HookHandlerInterface;
-use RubenMartinDev\PrestaShopModuleInstaller\Handler\Tab\TabHandler;
 use RubenMartinDev\PrestaShopModuleInstaller\Handler\Tab\TabHandlerFactory;
 use RubenMartinDev\PrestaShopModuleInstaller\Handler\Tab\TabHandlerInterface;
 
 /**
- * @phpstan-import-type TQuery from DatabaseHandlerFactory as TDatabaseQuery
- * @phpstan-import-type TQueries from DatabaseHandlerFactory as TDatabaseQueries
- *
- * @phpstan-import-type THook from HookHandlerFactory as THooksHook
- * @phpstan-import-type THooks from HookHandlerFactory as THooksHooks
- *
- * @phpstan-import-type TTab from TabHandlerFactory as TTabsTab
- * @phpstan-import-type TTabs from TabHandlerFactory as TTabsTabs
+ * @phpstan-import-type TQueries from DatabaseHandlerFactory as TDatabase
+ * @phpstan-import-type THooks from HookHandlerFactory as THooks
+ * @phpstan-import-type TTabs from TabHandlerFactory as TTabs
  */
 class InstallerFactory
 {
     /**
      * @param Module $module
      * @param array{
-     *   database?: TDatabaseQueries,
-     *   hooks?: THooksHooks,
-     *   tabs?: TTabsTabs,
+     *   database?: TDatabase,
+     *   hooks?: THooks,
+     *   tabs?: TTabs,
      * } $handlers
-     * @param callable(Module $module, TDatabaseQuery $properties): DatabaseHandlerInterface|null $factoryDatabase
-     * @param callable(Module $module, THooksHook $properties): HookHandlerInterface|null $factoryHooks
-     * @param callable(Module $module, TTabsTab $properties): TabHandlerInterface|null $factoryTabs
+     * @param callable(Module $module, TDatabase $queries): DatabaseHandlerInterface|null $factoryDatabase
+     * @param callable(Module $module, THooks $hooks): HookHandlerInterface|null $factoryHooks
+     * @param callable(Module $module, TTabs $tabs): TabHandlerInterface|null $factoryTabs
      *
      * @return InstallerInterface
      */
@@ -43,24 +37,9 @@ class InstallerFactory
         $factoryHooks = null,
         $factoryTabs = null
     ) {
-        $factoryDatabase = \is_callable($factoryDatabase)
-            ? $factoryDatabase
-            : function (Module $module, array $properties) {
-                return DatabaseHandlerFactory::create($module, $properties);
-            }
-        ;
-        $factoryHooks = \is_callable($factoryHooks)
-            ? $factoryHooks
-            : function (Module $module, array $properties) {
-                return HookHandlerFactory::create($module, $properties);
-            }
-        ;
-        $factoryTabs = \is_callable($factoryTabs)
-            ? $factoryTabs
-            : function (Module $module, array $properties) {
-                return TabHandlerFactory::create($module, $properties);
-            }
-        ;
+        $factoryDatabase    = \is_callable($factoryDatabase)    ? $factoryDatabase  : [self::class, 'defaultDatabaseFactory'];
+        $factoryHooks       = \is_callable($factoryHooks)       ? $factoryHooks     : [self::class, 'defaultHooksFactory'];
+        $factoryTabs        = \is_callable($factoryTabs)        ? $factoryTabs      : [self::class, 'defaultTabsFactory'];
 
         foreach ($handlers as $name => &$properties) {
             if ('database' === $name) {
@@ -79,5 +58,38 @@ class InstallerFactory
         $handlers = \array_values($handlers);
 
         return new Installer($handlers);
+    }
+
+    /**
+     * @param Module $module
+     * @param TDatabase $queries
+     *
+     * @return DatabaseHandlerInterface
+     */
+    private static function defaultDatabaseFactory(Module $module, array $queries)
+    {
+        return DatabaseHandlerFactory::create($module, $queries);
+    }
+
+    /**
+     * @param Module $module
+     * @param THooks $hooks
+     *
+     * @return HookHandlerInterface
+     */
+    private static function defaultHooksFactory(Module $module, array $hooks)
+    {
+        return HookHandlerFactory::create($module, $hooks);
+    }
+
+    /**
+     * @param Module $module
+     * @param TTabs $tabs
+     *
+     * @return TabHandlerInterface
+     */
+    private static function defaultTabsFactory(Module $module, array $tabs)
+    {
+        return TabHandlerFactory::create($module, $tabs);
     }
 }
--- tests/InstallerFactoryTest.php
+++ tests/InstallerFactoryTest.php
@@ -6,12 +6,50 @@ use Module;
 use org\bovigo\vfs\vfsStream;
 use PHPUnit_Framework_MockObject_MockObject;
 use PHPUnit\Framework\TestCase;
+use RubenMartinDev\PrestaShopModuleInstaller\Handler\Database\DatabaseHandlerInterface;
 use RubenMartinDev\PrestaShopModuleInstaller\Handler\HandlerInstallerInterface;
+use RubenMartinDev\PrestaShopModuleInstaller\Handler\Hook\HookHandlerInterface;
+use RubenMartinDev\PrestaShopModuleInstaller\Handler\Tab\TabHandlerInterface;
 use RubenMartinDev\PrestaShopModuleInstaller\InstallerFactory;
 use RubenMartinDev\PrestaShopModuleInstaller\InstallerInterface;
 
 class InstallerFactoryTest extends TestCase
 {
+    public function testCreateReturnsInstallerWithCallbacks()
+    {
+        $factoryDatabase = function () {
+            return $this->createHandlerInstallerMock(DatabaseHandlerInterface::class);
+        };
+        $factoryHooks = function () {
+            return $this->createHandlerInstallerMock(HookHandlerInterface::class);
+        };
+        $factoryTabs = function () {
+            return $this->createHandlerInstallerMock(TabHandlerInterface::class);
+        };
+
+        $installer = InstallerFactory::create(
+            $this->createModuleMock(),
+            [
+                'database'  => [],
+                'hooks'     => [],
+                'tabs'      => [],
+            ],
+            $factoryDatabase,
+            $factoryHooks,
+            $factoryTabs
+        );
+
+        $this->assertInstanceOf(InstallerInterface::class, $installer);
+
+        $handler1 = $installer->getHandler(0);
+        $handler2 = $installer->getHandler(1);
+        $handler3 = $installer->getHandler(2);
+
+        $this->assertInstanceOf(DatabaseHandlerInterface::class, $handler1);
+        $this->assertInstanceOf(HookHandlerInterface::class, $handler2);
+        $this->assertInstanceOf(TabHandlerInterface::class, $handler3);
+    }
+
     public function testCreateReturnsInstallerWithoutCallbacks()
     {
         $directory = vfsStream::setup();
@@ -50,57 +88,19 @@ class InstallerFactoryTest extends TestCase
         $handler2 = $installer->getHandler(1);
         $handler3 = $installer->getHandler(2);
 
-        $this->assertInstanceOf(HandlerInstallerInterface::class, $handler1);
-        $this->assertInstanceOf(HandlerInstallerInterface::class, $handler2);
-        $this->assertInstanceOf(HandlerInstallerInterface::class, $handler3);
-    }
-
-    public function testCreateReturnsInstallerWithCallbacks()
-    {
-        $factoryDatabase = function () {
-            return $this->createHandlerInstallerMock();
-        };
-        $factoryHooks = function () {
-            return $this->createHandlerInstallerMock();
-        };
-        $factoryTabs = function () {
-            return $this->createHandlerInstallerMock();
-        };
-
-        $installer = InstallerFactory::create(
-            $this->createModuleMock(),
-            [
-                'database'  => [],
-                'hooks'     => [],
-                'tabs'      => [],
-            ],
-            $factoryDatabase,
-            $factoryHooks,
-            $factoryTabs
-        );
-
-        $this->assertInstanceOf(InstallerInterface::class, $installer);
-
-        $handler1 = $installer->getHandler(0);
-        $handler2 = $installer->getHandler(1);
-        $handler3 = $installer->getHandler(2);
-
-        $this->assertInstanceOf(HandlerInstallerInterface::class, $handler1);
-        $this->assertInstanceOf(HandlerInstallerInterface::class, $handler2);
-        $this->assertInstanceOf(HandlerInstallerInterface::class, $handler3);
+        $this->assertInstanceOf(DatabaseHandlerInterface::class, $handler1);
+        $this->assertInstanceOf(HookHandlerInterface::class, $handler2);
+        $this->assertInstanceOf(TabHandlerInterface::class, $handler3);
     }
 
     /**
+     * @param class-string $className
+     *
      * @return HandlerInstallerInterface|PHPUnit_Framework_MockObject_MockObject
      */
-    private function createHandlerInstallerMock()
+    private function createHandlerInstallerMock($className)
     {
-        $handler = $this->createMock(HandlerInstallerInterface::class);
-
-        $handler->method('install')->willReturn(true);
-        $handler->method('uninstall')->willReturn(true);
-
-        return $handler;
+        return $this->createMock($className);
     }
 
     /**
